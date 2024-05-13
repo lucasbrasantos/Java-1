c6663f795a15f967984fe79ec0dd2d5253c0da94
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-05-13 12:15:08 -0300

    Create OtherOperators.java

---
+++ module3/lesson6/OtherOperators.java
@@ -0,0 +1,19 @@
+package module3.lesson6;
+
+public class OtherOperators {
+    
+    public static void main(String[] args) {
+        
+        int sundayFahrenheit = 80;
+        int saturdayFahrenheit = 110;
+        int MIN_TEMP = 95;
+
+        if ((saturdayFahrenheit + sundayFahrenheit) / 2 >= MIN_TEMP) {
+            System.out.println("Yay! Nice weekend average.");
+        }
+
+
+        
+
+    }
+}
